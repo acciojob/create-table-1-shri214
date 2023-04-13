54643800d8932b5557b1b42d40d7a63bd1c23fdb
Shriram kushwaha <117708615+shri214@users.noreply.github.com>
2023-04-13 13:00:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+    var i=3;
+	var j=1;
+	var k=2;
 function insert_Row() {
     //Write your code here
-  
-  
+	
+  let str=`<tr><td>Row${i} cell${j}</td><td>Row${i} cell${k}</td></tr>`
+	var table=document.querySelector("table");
+	table.innerHTML+=str;
+	i++;
 }
